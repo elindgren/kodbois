ea4ad537d4b6b8b08e5b7c25da4da24ac1dc7054
m-ulmestrand <54723095+m-ulmestrand@users.noreply.github.com>
2019-08-30 21:21:27 +0200

    Update slumpnamn.py

--- slumpnamn.py
+++ slumpnamn.py
@@ -11,24 +11,22 @@ def main(argv):
     separator = ["", " ", "/", "\\", "-", "_", "&", "@"]
 
     def print_names(n):
-        new_name = ""
         for i in range(n):
+            new_name = ""
             nr_word = 2 + int(np.random.uniform(-2, 2))
             for j in range(nr_word):
                 new_name += random.choice(word)
                 if not j == nr_word - 1:
                     new_name += random.choice(separator)
             print(new_name)
-            new_name = ""
-    
 
-    while True:
-        try:
-            num = int(input("Enter number of desired names: "))
-            print_names(num)
-            break
-        except ValueError:
-            print("Entered value is not an int.")
+
+    try:
+        # num = int(input("Enter number of desired names: "))
+        num = int(argv[1])
+        print_names(num)
+    except:
+        print_names(1)
 
     return 0
 
